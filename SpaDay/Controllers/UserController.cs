60278b3ce1446571ede86659a129ba6798ae5900
Daryn DeMoss <darynbrennondemoss@gmail.com>
2020-06-25 09:55:35 -0400

    Final commits

---
+++ SpaDay/Controllers/UserController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SpaDay.Models;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace SpaDay.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: /user
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: /user/add
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        // POST: /user/add
+        [HttpPost("/user/add")]
+        public IActionResult Create(User user, string verify)
+        {
+            if (user.Password == verify)
+            {
+                ViewBag.user = user;
+                return View("Index");
+            }
+            else
+            {
+                ViewBag.user = user;
+                ViewBag.error = "Passwords didn't match !";
+                return View("Add");
+            }
+        }
+    }
+}
